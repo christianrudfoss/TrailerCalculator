61214fed4243acb130224437a42d4f88859e7c41
Christian Rudfoss <christian@knowledgegroup.no>
2022-03-01 22:49:33 +0100

    update4

--- TrailorCalculator/Program.cs
+++ TrailorCalculator/Program.cs
@@ -11,19 +11,19 @@ namespace TrailorCalculator
             Trailer trailerKY6490 = new() { TrailerNumber = "KY6490", DeadWeight = 1076 };
             Trailer trailerAZ8056 = new() { TrailerNumber = "AZ8056", DeadWeight = 155 };
 
-            Console.WriteLine($"Task 1A: Class 'B' legal with {trailerKY6490.TrailerNumber} (car weight: {carEV66684.TotalWeight}kg. Trailer weight: {trailerKY6490.DeadWeight + 2424}kg): " +
+            Console.WriteLine($"Task 1A:    Class 'B'   legal with {trailerKY6490.TrailerNumber} (car weight: {carEV66684.TotalWeight}kg. Trailer weight: {trailerKY6490.DeadWeight + 2424}kg) => " +
                 $"{TrailerCalculatorProcessor.ValidDriverLicence("B", carEV66684.TotalWeight, trailerKY6490.DeadWeight + 2424)}");
 
-            Console.WriteLine($"Task 1B: Class 'BE' legal with {trailerKY6490.TrailerNumber} (car weight: {carEV66684.TotalWeight}kg. Trailer weight: {trailerKY6490.DeadWeight + 2424}kg): " +
+            Console.WriteLine($"Task 1B:    Class 'BE'  legal with {trailerKY6490.TrailerNumber} (car weight: {carEV66684.TotalWeight}kg. Trailer weight: {trailerKY6490.DeadWeight + 2424}kg) => " +
                 $"{TrailerCalculatorProcessor.ValidDriverLicence("BE", carEV66684.TotalWeight, trailerKY6490.DeadWeight + 2424)}");
 
-            Console.WriteLine($"Task 2:  Class 'B' legal with {trailerAZ8056.TrailerNumber} (car weight: {carEV66684.TotalWeight}kg. Trailer weight: {trailerAZ8056.DeadWeight + 510}kg): " +
+            Console.WriteLine($"Task 2:     Class 'B'   legal with {trailerAZ8056.TrailerNumber} (car weight: {carEV66684.TotalWeight}kg. Trailer weight: {trailerAZ8056.DeadWeight + 510}kg ) => " +
                 $"{TrailerCalculatorProcessor.ValidDriverLicence("B", carEV66684.TotalWeight, trailerAZ8056.DeadWeight + 510)}");
 
-            Console.WriteLine($"Task 3 (1): Class 'B96' legal with {trailerKY6490.TrailerNumber} (car weight: {carEV66684.TotalWeight}kg. Trailer weight: {trailerKY6490.DeadWeight + 2424}kg): " +
+            Console.WriteLine($"Task 3 (1): Class 'B96' legal with {trailerKY6490.TrailerNumber} (car weight: {carEV66684.TotalWeight}kg. Trailer weight: {trailerKY6490.DeadWeight + 2424}kg) => " +
                 $"{TrailerCalculatorProcessor.ValidDriverLicence("B96", carEV66684.TotalWeight, trailerKY6490.DeadWeight + 2424)}");
 
-            Console.WriteLine($"Task 3 (2): Class 'B96' legal with {trailerAZ8056.TrailerNumber} (car weight: {carEV66684.TotalWeight}kg. Trailer weight: {trailerAZ8056.DeadWeight + 510}kg): " +
+            Console.WriteLine($"Task 3 (2): Class 'B96' legal with {trailerAZ8056.TrailerNumber} (car weight: {carEV66684.TotalWeight}kg. Trailer weight: {trailerAZ8056.DeadWeight + 510}kg ) => " +
                 $"{TrailerCalculatorProcessor.ValidDriverLicence("B96", carEV66684.TotalWeight, trailerAZ8056.DeadWeight + 510)}");
         }
     }
